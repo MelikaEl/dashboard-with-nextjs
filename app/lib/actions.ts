20a2b119d013e6e7db3d57f321fc93c6cc59babb
MelikaEl <melika.melika8@gmail.com>
2024-10-15 13:37:54 +0330

    apply error handling for the server actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,11 +24,18 @@ export async function createInvoice(formData: FormData) {
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];//This line of code creates a date string in the format "YYYY-MM-DD" representing the current date. Let's break it down: 1. new Date() creates a new Date object representing the current date and time.2. .toISOString() converts the Date object to a string in ISO 8601 format. This format looks like: "YYYY-MM-DDTHH:mm:ss.sssZ" (e.g., "2023-04-15T12:30:45.678Z"). .split('T')[0] splits the ISO string at the 'T' character (which separates the date from the time in the ISO format) and takes the first part (index 0) of the resulting array. This effectively removes the time portion and keeps only the date part.The result is a string containing just the date in "YYYY-MM-DD" format, such as "2023-04-15".
-   
+   try{
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+   }
+   catch(error){
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
+   }
+    
    
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -54,13 +61,36 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
  
   const amountInCents = amount * 100;
- 
+ try{
   await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+  UPDATE invoices
+  SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+  WHERE id = ${id}
+`;
+ }
+ catch(error){
+  return{
+    message:'Database Error: Failed to Update Invoice.'
+  }
+ }
+ 
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+
+export async function deleteInvoice(id: string) {
+  // throw new Error('Failed to Delete Invoice');
+  try{
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice' };
+  }
+  catch(error){
+    return{
+      message:'Database Error: Failed to Delete Invoice.'
+    }
+  }
+  // revalidatePath('/dashboard/invoices');
 }
