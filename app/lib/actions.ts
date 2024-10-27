972f36deb7b7847892b29f728ec8c69487df2278
MelikaEl <melika.melika8@gmail.com>
2024-10-27 09:35:24 +0330

    remove error on the catch block because eslint errors for deploy on vercel

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -32,7 +32,7 @@ export async function createInvoice(formData: FormData) {
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
    }
-   catch(error){
+   catch{
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
@@ -70,7 +70,7 @@ export async function updateInvoice(id: string, formData: FormData) {
   WHERE id = ${id}
 `;
  }
- catch(error){
+ catch{
   return{
     message:'Database Error: Failed to Update Invoice.'
   }
@@ -89,7 +89,7 @@ export async function deleteInvoice(id: string) {
     revalidatePath('/dashboard/invoices');
     return { message: 'Deleted Invoice' };
   }
-  catch(error){
+  catch{
     return{
       message:'Database Error: Failed to Delete Invoice.'
     }
