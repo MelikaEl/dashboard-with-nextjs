91ca71039afbbe3f0353b208255ddfe1fd8005cf
MelikaEl <melika.melika8@gmail.com>
2024-10-27 09:11:58 +0330

    add zod type for the objects of the form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,6 +6,9 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
+    customerId:z.string(),
+    amount:z.coerce.number(),
+    status:z.enum(['pending','paid']),
     date: z.string(),
   });
 
