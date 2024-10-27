2f2804d04e4d3ceaf111ba16fc3216675199ac7a
MelikaEl <melika.melika8@gmail.com>
2024-10-27 09:00:39 +0330

    remove validation from action file

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,15 +4,6 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-export type State={
-  errors?:{// optional property
-    customerId?:string[];
-    amount?:string[];
-    status?:string[];
-  };
-  message?:string|null;
-}
-
 const FormSchema = z.object({
     id: z.string(),
     date: z.string(),
@@ -21,24 +12,15 @@ const FormSchema = z.object({
   const CreateInvoice = FormSchema.omit({ id: true, date: true });
  
 
-export async function createInvoice(prevState: State,formData: FormData) {
-    // const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
     //Validate from fields using zod
-      const validatedFields = CreateInvoice.safeParse({
+     
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
-     // If form validation fails, return errors early. Otherwise, continue.
-  if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors,
-      message: 'Missing Fields. Failed to Create Invoice.',
-      
-    };
-    
-  }
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];//This line of code creates a date string in the format "YYYY-MM-DD" representing the current date. Let's break it down: 1. new Date() creates a new Date object representing the current date and time.2. .toISOString() converts the Date object to a string in ISO 8601 format. This format looks like: "YYYY-MM-DDTHH:mm:ss.sssZ" (e.g., "2023-04-15T12:30:45.678Z"). .split('T')[0] splits the ISO string at the 'T' character (which separates the date from the time in the ISO format) and takes the first part (index 0) of the resulting array. This effectively removes the time portion and keeps only the date part.The result is a string containing just the date in "YYYY-MM-DD" format, such as "2023-04-15".
    try{
